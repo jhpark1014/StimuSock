ca44f0ed8c5d1651a9dfb6b8451accd59046e72f
jhpark1014 <jhpark981014@gmail.com>
2023-01-28 16:31:50 -0600

    Navigate to All Pages

--- StimuSock/ContentView.swift
+++ StimuSock/ContentView.swift
@@ -9,20 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image("SockIcon")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("StimuSock")
-            Button("Start") {
-//                Connects to bluetooth
+        NavigationView {
+            VStack {
+                Image("SockIcon")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("StimuSock")
+                NavigationLink(destination: HomePage()) {
+                    Text("Start")
+                    
+                }
             }
-            .frame(width: 50.0)
-            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
-            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.white/*@END_MENU_TOKEN@*/)
+            .padding()
+            .bold()
         }
-        .padding()
-        .bold()
     }
 }
 
