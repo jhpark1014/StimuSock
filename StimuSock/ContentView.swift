edf11e0ddf55fa5d3f23797b1c0a1f1194f2f3fc
jhpark1014 <jhpark981014@gmail.com>
2023-01-28 14:36:50 -0600

    Initial Commit

---
+++ StimuSock/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  StimuSock
+//
+//  Created by Sarah Park on 1/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("SockIcon")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("StimuSock")
+            Button("Start") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .frame(width: 50.0)
+            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.white/*@END_MENU_TOKEN@*/)
+        }
+        .padding()
+        .bold()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StimuSock/StimuSockApp.swift
@@ -0,0 +1,17 @@
+//
+//  StimuSockApp.swift
+//  StimuSock
+//
+//  Created by Sarah Park on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct StimuSockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
